63ce925550d5e1882e20ad2c0108d03520a058c8
Divyesh Markana <divyeshmarkana724@gmail.com>
2022-01-08 15:38:36 +0530

    add operator key and add/remove is-depressed class

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ keys.addEventListener("click", e => {
         const keyContent = key.textContent
         const displayValue = display.textContent
 
+         // Remove .is-depressed class from all keys
+    Array.from(key.parentNode.children)
+    .forEach(k => k.classList.remove('is-depressed'))
+
         // replace displayValue 0 to user presees key
         if (!action) {
             if (displayValue === "0") {
@@ -19,8 +23,15 @@ keys.addEventListener("click", e => {
             }
         }
 
+        // add decimal key and concatenate with display value
         if (action === "decimal") {
             display.textContent = displayValue + "."
+        }
+
+        if (action === "add" || action === "subtract" || action === "mutiply" || action === "divide") {
+
+            key.classList.add("is-depressed")
+            calculator.dataset.previousKeyType = "operator"
         }
     }
 })
