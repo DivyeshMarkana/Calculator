2ae1140ece3b4ba771be2e7b020b8faa49afddc9
Divyesh Markana <divyeshmarkana724@gmail.com>
2022-01-08 16:22:00 +0530

    add parseFloat function to convert string to numbr

--- index.js
+++ index.js
@@ -2,24 +2,25 @@ const calculator = document.querySelector(".calculator")
 const display = document.querySelector(".calculator__display")
 const keys = document.querySelector(".calculator__keys")
 
+
+// creates a calculate function for calculates value
 const calculate = (n1, operator, n2) => {
     let result = ""
 
     if (operator === "add") {
-        result = n1 + n2
+        result = parseFloat(n1) + parseFloat(n2)
     }
     else if (operator === "subtract") {
-        result = n1 - n2
+        result = parseFloat(n1) - parseFloat(n2)
     }
     else if (operator === "multiply") {
-        result = n1 * n2
+        result = parseFloat(n1) * parseFloat(n2)
     }
     else if (operator === "divide") {
-        result = n1 / n2
+        result = parseFloat(n1) / parseFloat(n2)
     }
     return result
 }
-
 
 keys.addEventListener("click", e => {
     if (e.target.matches("button")) {
@@ -28,9 +29,6 @@ keys.addEventListener("click", e => {
         const keyContent = key.textContent
         const displayValue = display.textContent
 
-        // Remove .is-depressed class from all keys
-        Array.from(key.parentNode.children)
-            .forEach(k => k.classList.remove('is-depressed'))
 
 
         const previousKeyType = calculator.dataset.previousKeyType
@@ -58,6 +56,10 @@ keys.addEventListener("click", e => {
             calculator.dataset.firstValue = displayValue
             calculator.dataset.operator = action
         }
+        // Remove .is-depressed class from all keys
+        Array.from(key.parentNode.children)
+            .forEach(k => k.classList.remove('is-depressed'))
+
 
         if (action === "calculate") {
             const firstValue = calculator.dataset.firstValue
